a54de2eac2a4d4af60caf8786867348d89799d18
frhn9 <just_fadhyl@hotmail.co.id>
2023-01-24 22:02:44 +0700

    refactor: refactor mocking error

--- src/test/java/org/ait/project/guideline/example/modules/post/controller/JsonPlaceHolderPostControllerTest.java
+++ src/test/java/org/ait/project/guideline/example/modules/post/controller/JsonPlaceHolderPostControllerTest.java
@@ -1,9 +1,17 @@
 package org.ait.project.guideline.example.modules.post.controller;
 
+import org.ait.project.guideline.example.modules.post.model.entity.JsonPlaceHolderPost;
 import org.ait.project.guideline.example.modules.post.model.repository.JsonPlaceHolderPostRepository;
+import org.ait.project.guideline.example.modules.post.service.delegate.JsonPlaceHolderPostDelegate;
+import org.ait.project.guideline.example.modules.post.service.delegate.impl.JsonPlaceHolderPostDelegateImpl;
 import org.ait.project.guideline.example.shared.openfeign.jsonplaceholder.JsonPlaceHolderClient;
+import org.ait.project.guideline.example.shared.openfeign.jsonplaceholder.request.CreatePostRequest;
+import org.ait.project.guideline.example.shared.openfeign.jsonplaceholder.response.PostResponse;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +22,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+
 /**
  * We use @ActiveProfiles("test"), because :
  * <ul>
@@ -22,6 +35,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
  * </ul>
  *
  * @see resources/application-test.yml
+ * @author @fadhylfa / @frhn9
  */
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,9 +43,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 class JsonPlaceHolderPostControllerTest {
 
   /**
-   * We use Annotation @Autowired for H2 Database, dont worry, it will use separate database.
+   * Dont worry about database, we use H2 database as defined at application-test.yaml.
    * <br>
-   * but, for OpenFeign Client, we mock it using Annotation @MockBean
+   * but, for OpenFeign Client, we mock it using Annotation @MockBean to mock it
    */
   @Autowired
   private MockMvc mockMvc;
@@ -39,25 +53,34 @@ class JsonPlaceHolderPostControllerTest {
   @MockBean
   private JsonPlaceHolderClient jsonPlaceHolderClient;
 
-  @Autowired
-  private JsonPlaceHolderPostRepository jsonPlaceHolderPostRepository;
-
   /**
    * This method will test endpoint GET: {url}/post
    * <br>Expected result is the endpoint will returning field response_schema, response_code, and response_message
    *
    * @throws Exception
    * @see <a href="https://documenter.getpostman.com/view/25201895/2s8Z73xVro#ef29d6fd-c193-4935-88ee-61a2077f782a">Postman API link</a>
+   * @author @fadhylfa / @frhn9
    */
   @Test
   void getAllJsonPlaceHolderPost() throws Exception {
+    PostResponse postResponse = new PostResponse();
+    postResponse.setId(1);
+    postResponse.setTitle("title");
+    postResponse.setBody("body");
+    postResponse.setUserId(1);
+    List<PostResponse> postResponseList = Collections.singletonList(postResponse);
+
+    Mockito.when(jsonPlaceHolderClient.getListPost()).thenReturn(postResponseList);
+
     mockMvc.perform(MockMvcRequestBuilders.get("/post")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("response_schema")))
         .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("response_code")))
-        .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("response_message")));
+        .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("response_message")))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.list.content").isArray())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.list.content", Matchers.hasSize(1)));
   }
 
   @Test
@@ -77,16 +100,30 @@ class JsonPlaceHolderPostControllerTest {
    *
    * @throws Exception
    * @see <a href="https://documenter.getpostman.com/view/25201895/2s8Z73xVro#4311eb97-078e-442f-a2c2-e625d480a2b2">Postman API link</a>
+   * @author @fadhylfa / @frhn9
    */
   @Test
   void addPost() throws Exception {
+    CreatePostRequest postReq = new CreatePostRequest();
+    postReq.setTitle("test test");
+    postReq.setBody("testing@gmail.com");
+    postReq.setUserId(777);
+
+    PostResponse postResponse = new PostResponse();
+    postResponse.setId(1);
+    postResponse.setTitle("test test");
+    postResponse.setBody("testing@gmail.com");
+    postResponse.setUserId(777);
+
+    Mockito.when(jsonPlaceHolderClient.createPost(postReq)).thenReturn(postResponse);
+
     mockMvc.perform(MockMvcRequestBuilders.post("/post")
             .contentType(MediaType.APPLICATION_JSON)
         // code below is for request body
             .content("{\n" +
                 "    \"title\": \"test test\",\n" +
                 "    \"body\": \"testing@gmail.com\",\n" +
-                "    \"userId\": \"777\"\n" +
+                "    \"userId\": \"666\"\n" +
                 "}"))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(
@@ -98,7 +135,11 @@ class JsonPlaceHolderPostControllerTest {
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema").exists())
         // code below expected to check content response
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.title").exists())
-        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.title").value("test test"));
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.title").value("test test"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.body").exists())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.body").value("testing@gmail.com"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.userId").exists())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.userId").value("666"));
   }
 
 }
