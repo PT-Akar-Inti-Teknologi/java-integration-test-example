43ce3fe44107d3876563c3145d5b281a542fc4f6
frhn9 <just_fadhyl@hotmail.co.id>
2023-01-24 23:17:07 +0700

    feat: add example integration test using @Query annotations

--- src/main/java/org/ait/project/guideline/example/modules/post/model/repository/JsonPlaceHolderPostRepository.java
+++ src/main/java/org/ait/project/guideline/example/modules/post/model/repository/JsonPlaceHolderPostRepository.java
@@ -2,8 +2,15 @@ package org.ait.project.guideline.example.modules.post.model.repository;
 
 import org.ait.project.guideline.example.modules.post.model.entity.JsonPlaceHolderPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface JsonPlaceHolderPostRepository extends JpaRepository<JsonPlaceHolderPost,Integer> {
+
+    @Query("SELECT json from JsonPlaceHolderPost json where json.id = ?1")
+    Optional<JsonPlaceHolderPost> findById(Integer id);
+
 }
--- src/test/java/org/ait/project/guideline/example/modules/post/controller/JsonPlaceHolderPostControllerTest.java
+++ src/test/java/org/ait/project/guideline/example/modules/post/controller/JsonPlaceHolderPostControllerTest.java
@@ -2,15 +2,13 @@ package org.ait.project.guideline.example.modules.post.controller;
 
 import org.ait.project.guideline.example.modules.post.model.entity.JsonPlaceHolderPost;
 import org.ait.project.guideline.example.modules.post.model.repository.JsonPlaceHolderPostRepository;
-import org.ait.project.guideline.example.modules.post.service.delegate.JsonPlaceHolderPostDelegate;
-import org.ait.project.guideline.example.modules.post.service.delegate.impl.JsonPlaceHolderPostDelegateImpl;
 import org.ait.project.guideline.example.shared.openfeign.jsonplaceholder.JsonPlaceHolderClient;
 import org.ait.project.guideline.example.shared.openfeign.jsonplaceholder.request.CreatePostRequest;
 import org.ait.project.guideline.example.shared.openfeign.jsonplaceholder.response.PostResponse;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,10 +20,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
 
 /**
  * We use @ActiveProfiles("test"), because :
@@ -53,6 +50,21 @@ class JsonPlaceHolderPostControllerTest {
   @MockBean
   private JsonPlaceHolderClient jsonPlaceHolderClient;
 
+  @Autowired
+  JsonPlaceHolderPostRepository jsonPlaceHolderPostRepository;
+
+  @BeforeEach
+  void setup() {
+    JsonPlaceHolderPost jsonPlaceHolderPost = new JsonPlaceHolderPost();
+    jsonPlaceHolderPost.setId(1);
+    jsonPlaceHolderPost.setTitle("hanya sebuah title");
+    jsonPlaceHolderPost.setBody("sexeh");
+    jsonPlaceHolderPost.setUserId(666);
+    jsonPlaceHolderPost.setCreateAt(ZonedDateTime.now());
+
+    jsonPlaceHolderPostRepository.save(jsonPlaceHolderPost);
+  }
+
   /**
    * This method will test endpoint GET: {url}/post
    * <br>Expected result is the endpoint will returning field response_schema, response_code, and response_message
@@ -62,11 +74,14 @@ class JsonPlaceHolderPostControllerTest {
    * @author @fadhylfa / @frhn9
    */
   @Test
-  void getAllJsonPlaceHolderPost() throws Exception {
+  @Order(1)
+  void get_all_jsonplaceholder_post() throws Exception {
+    jsonPlaceHolderPostRepository.deleteAll();
+
     PostResponse postResponse = new PostResponse();
     postResponse.setId(1);
-    postResponse.setTitle("title");
-    postResponse.setBody("body");
+    postResponse.setTitle("sebuah title");
+    postResponse.setBody("sebuah body");
     postResponse.setUserId(1);
     List<PostResponse> postResponseList = Collections.singletonList(postResponse);
 
@@ -84,18 +99,15 @@ class JsonPlaceHolderPostControllerTest {
   }
 
   @Test
+  @Order(2)
   void getAllJsonPlaceHolderPostPage() {
   }
 
-  @Test
-  void getJsonPlaceHolderPostById() {
-  }
-
-  /**
-   * This method will test endpoint POST: {url}/post
+   /**
+   * This method will test endpoint GET: {url}/{id}
    * <ul>
-   * <li>For request body, you must convert your object to json string first</li>
-   * <li>Expected result is to check whether default response content response is expected as we define at test method</li>
+   * <li>This endpoint use annotation @Query for repository method</li>
+   * <li>Expected result is success so it will return HTTP 200 OK</li>
    * </ul>
    *
    * @throws Exception
@@ -103,7 +115,48 @@ class JsonPlaceHolderPostControllerTest {
    * @author @fadhylfa / @frhn9
    */
   @Test
-  void addPost() throws Exception {
+  @Order(3)
+  void get_jsonplaceholderpost_by_id_when_found() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/1")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  /**
+   * This method will test endpoint GET: {url}/{id}
+   * <ul>
+   * <li>This endpoint use annotation @Query for repository method</li>
+   * <li>Expected result is success so it will return HTTP 404 Not Found</li>
+   * <li>Expected json body is to check whether content response is expected as we define at test method</li>
+   * </ul>
+   *
+   * @throws Exception
+   * @see <a href="https://documenter.getpostman.com/view/25201895/2s8Z73xVro#4311eb97-078e-442f-a2c2-e625d480a2b2">Postman API link</a>
+   * @author @fadhylfa / @frhn9
+   */
+  @Test
+  @Order(4)
+  void get_jsonplaceholderpost_by_id_when_not_found() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/666")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema.response_code").value("PMRK-0001"));
+  }
+
+  /**
+   * This method will test endpoint GET: {url}/{id}
+   * <ul>
+   * <li>Expected result is data not found so it will return HTTP 404 Not Found</li>
+   * <li>Expected json body is to check whether content response is expected as we define at test method</li>
+   * </ul>
+   *
+   * @throws Exception
+   * @see <a href="https://documenter.getpostman.com/view/25201895/2s8Z73xVro#4311eb97-078e-442f-a2c2-e625d480a2b2">Postman API link</a>
+   * @author @fadhylfa / @frhn9
+   */
+  @Test
+  @Order(5)
+  void add_post_when_success() throws Exception {
     CreatePostRequest postReq = new CreatePostRequest();
     postReq.setTitle("test test");
     postReq.setBody("testing@gmail.com");
@@ -126,8 +179,7 @@ class JsonPlaceHolderPostControllerTest {
                 "    \"userId\": \"666\"\n" +
                 "}"))
         .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(
-            MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
         // code below expected to check default response
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema").exists())
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema.response_code").exists())
@@ -140,6 +192,40 @@ class JsonPlaceHolderPostControllerTest {
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.body").value("testing@gmail.com"))
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.userId").exists())
         .andExpect(MockMvcResultMatchers.jsonPath("$.response_output.detail.userId").value("666"));
+  }
+
+  /**
+   * This method will test endpoint POST: {url}/post
+   * <ul>
+   * <li>For request body, you must convert your object to json string first</li>
+   * <li>Expected result is failed so it will return HTTP 400 Bad Request</li>
+   * <li>Expected json body is to check whether content response is expected as we define at test method</li>
+   * </ul>
+   *
+   * @throws Exception
+   * @see <a href="https://documenter.getpostman.com/view/25201895/2s8Z73xVro#4311eb97-078e-442f-a2c2-e625d480a2b2">Postman API link</a>
+   * @author @fadhylfa / @frhn9
+   */
+  @Test
+  @Order(6)
+  void add_post_when_validation_error() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/post")
+            .contentType(MediaType.APPLICATION_JSON)
+            // code below is for request body
+            .content("{\n" +
+                "    \"title\": \"test test\",\n" +
+                "    \"body\": \"testing@gmail.com\",\n" +
+                "    \"userId\": \"\"\n" +
+                "}"))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        // code below expected to check default response
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema").exists())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema.response_code").exists())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema.response_code").value("PMRK-0009"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema.response_message").exists())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema.response_message").value("invalid parameter"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.response_schema").exists());
   }
 
 }
